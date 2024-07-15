7c91a65c512fa9b62854073d63b719fe7656fced
BeatrizVenturi <venturibeatriz329@gmail.com>
2024-07-15 11:53:51 -0300

    criação da pag login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,10 @@
-<script setup></script>
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
 <template>
-  <h1>Página de Login</h1>
+  <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth app-id="r0Ee7CaXSSQoNEv99PqfsReI"></passage-auth>
+  </div>
 </template>
